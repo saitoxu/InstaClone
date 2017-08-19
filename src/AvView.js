77ce7e6f83201ba257370053811cbd3fe34a3d8a
Yosuke Saito <yosuke.saito1202@gmail.com>
2017-08-19 23:17:05 +0900

    update AvView

--- src/AvView.js
+++ src/AvView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Image, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Image, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
 import Video from 'react-native-video'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
@@ -21,12 +21,31 @@ export default class AvView extends Component {
     imageHeight: 0
   }
 
+  constructor(props) {
+    super(props);
+    this.onLoad = this.onLoad.bind(this);
+    this.onProgress = this.onProgress.bind(this);
+    this.onBuffer = this.onBuffer.bind(this);
+  }
+
   componentWillMount() {
     if (this.props.type === 'image') {
       Image.getSize(this.props.source, (w, h) => {
         this.setState({ imageHeight: Math.floor(h * (width / w)) })
       })
     }
+  }
+
+  onLoad(data) {
+    this.setState({ duration: data.duration });
+  }
+
+  onProgress(data) {
+    this.setState({ currentTime: data.currentTime });
+  }
+
+  onBuffer({ isBuffering }: { isBuffering: boolean }) {
+    this.setState({ isBuffering });
   }
 
   render() {
@@ -43,21 +62,30 @@ export default class AvView extends Component {
     }
 
     return (
-      <Video
-        source={{ uri: this.props.source }}
-        style={{ height: width }}
-        rate={this.state.rate}
-        paused={this.state.paused}
-        volume={this.state.volume}
-        muted={this.state.muted}
-        ignoreSilentSwitch={this.state.ignoreSilentSwitch}
-        resizeMode={'cover'}
-        onLoad={this.onLoad}
-        onBuffer={this.onBuffer}
-        onProgress={this.onProgress}
-        onEnd={() => null}
-        repeat={true}
-      />
+      <TouchableOpacity
+        onPress={() => {this.setState({ paused: !this.state.paused })}}
+        activeOpacity={0.8}
+        style={{ width, height: width }}
+      >
+        <Video
+          source={{ uri: this.props.source }}
+          style={{ width, height: width }}
+          rate={this.state.rate}
+          paused={this.state.paused}
+          volume={this.state.volume}
+          muted={this.state.muted}
+          ignoreSilentSwitch={this.state.ignoreSilentSwitch}
+          resizeMode={'cover'}
+          onLoad={this.onLoad}
+          onBuffer={this.onBuffer}
+          onProgress={this.onProgress}
+          onEnd={() => null}
+          repeat={true}
+        />
+      <View style={{ position: 'absolute', right: 10, top: 10, backgroundColor: 'rgba(0, 0, 0, 0.6)', height: 40, width: 40, borderRadius: 20 }}>
+        <Ionicons name="ios-videocam-outline" size={24} color="white" style={{ backgroundColor: 'transparent', lineHeight: 40, marginLeft: 10 }} />
+      </View>
+      </TouchableOpacity>
     )
   }
 }
